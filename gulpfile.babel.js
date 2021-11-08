4c3e1dbe600455910766b8b4abaef51dd289da76
Britz Wang <britz.wang@kooppi.com>
2021-11-08 13:49:23 +0800

    Initial commit

---
+++ gulpfile.babel.js
@@ -0,0 +1,135 @@
+const { src, dest, series, parallel } = require('gulp')
+const clean = require('gulp-clean')
+const pug = require('gulp-pug')
+const prettyHtml = require('gulp-pretty-html')
+const babel = require('gulp-babel')
+const uglify = require('gulp-uglify')
+const sass = require('gulp-sass')
+const rename = require('gulp-rename')
+const injectHtml = require('gulp-inject-in-html')
+const server = require('gulp-webserver')
+const watch = require('gulp-watch')
+sass.compiler = require('node-sass')
+
+// clean all files in dist folder
+const cleanDist = () => {
+  return src('dist', {
+    allowEmpty: true,
+    sourcemaps: true,
+  }).pipe(clean())
+}
+
+// move files to dist folder
+const moveFiles = () => {
+  return src('public/**/*', {
+    sourcemaps: true,
+  }).pipe(dest('dist/'))
+}
+
+// compile pug to html and output
+const pugCompile = () => {
+  return src('src/**/*.pug', {
+    sourcemaps: true,
+  })
+    .pipe(pug())
+    .pipe(
+      prettyHtml({
+        indent_size: 2,
+        indent_char: ' ',
+        unformatted: true,
+        extra_liners: [],
+      })
+    )
+    .pipe(
+      rename({
+        dirname: '',
+      })
+    )
+    .pipe(dest('dist/'))
+}
+
+// compile next gen js to es5 and then minify to output folder
+const jsCompile = () => {
+  return src('src/**/*.js', {
+    sourcemaps: true,
+  })
+    .pipe(
+      babel({
+        presets: ['@babel/preset-env'],
+      })
+    )
+    .pipe(uglify())
+    .pipe(
+      rename({
+        dirname: '',
+        extname: '.min.js',
+      })
+    )
+    .pipe(dest('dist/assets/js/'))
+}
+
+// compile sass/scss to css and then minify to output folder
+const cssCompile = () => {
+  return src(['src/**/*.sass', 'src/**/*.scss'], {
+    sourcemaps: true,
+  })
+    .pipe(
+      sass({
+        outputStyle: 'compact',
+      })
+    )
+    .pipe(
+      rename({
+        dirname: '',
+        extname: '.min.css',
+      })
+    )
+    .pipe(dest('dist/assets/css/'))
+}
+
+// inject js/css/html into html
+const inject = () => {
+  return src('dist/**/*.html', {
+    sourcemaps: true,
+  })
+    .pipe(injectHtml())
+    .pipe(
+      prettyHtml({
+        indent_size: 2,
+        indent_char: ' ',
+        unformatted: true,
+        extra_liners: [],
+      })
+    )
+    .pipe(dest('dist/'))
+}
+
+// config webserver
+const webServer = () => {
+  return src('dist/').pipe(
+    server({
+      host: 'localhost',
+      port: 8000,
+      livereload: true,
+      open: true,
+    })
+  )
+}
+
+// watching action of adding/modifying/deleting files
+const watching = () => {
+  return watch(
+    'src/**/*',
+    { ignoreInitial: true },
+    series(cleanDist, moveFiles, parallel(pugCompile, jsCompile, cssCompile), inject)
+  )
+}
+
+exports.default = series(
+  cleanDist,
+  moveFiles,
+  parallel(pugCompile, jsCompile, cssCompile),
+  inject,
+  webServer,
+  watching
+)
---
+++ src/js/index.js
@@ -0,0 +1,78 @@
+$(function () {
+  // banner carousel
+  const bannerElem = $('.index-banner-carousel')
+  const bannerItems = $('.index-banner-carousel-item', bannerElem)
+  const bannerFlkty = new Flickity('.index-banner-carousel', {
+    wrapAround: true,
+    prevNextButtons: false,
+    on: {
+      ready: function () {
+        bannerElem.addClass('is-inited')
+        bannerItems.eq(this.slides.length - 1).addClass('is-prev')
+        bannerItems.eq(1).addClass('is-next')
+      }
+    }
+  })
+  bannerFlkty.on('change', function (index) {
+    bannerItems.removeClass('is-prev is-next')
+    const prevIndex = index === 0 ? bannerFlkty.slides.length - 1 : index - 1
+    const nextIndex = index + 1 === bannerFlkty.slides.length ? 0 : index + 1
+    bannerItems.eq(prevIndex).addClass('is-prev')
+    bannerItems.eq(nextIndex).addClass('is-next')
+  })
+
+  // extend carousel
+  const extendArrowPrev = $('.index-extend-arrow.prev')
+  const extendArrowNext = $('.index-extend-arrow.next')
+  const extendFlkty = new Flickity('.index-extend-list', {
+    draggable: false,
+    groupCells: 3,
+    cellAlign: 'left',
+    prevNextButtons: false,
+    on: {
+      ready: function () {
+        extendArrowPrev.attr('disabled', 'disabled')
+        if (this.slides.length <= 1) {
+          extendArrowNext.attr('disabled', 'disabled')
+        }
+      }
+    }
+  })
+  extendFlkty.on('change', function (index) {
+    extendArrowPrev.removeAttr('disabled')
+    extendArrowNext.removeAttr('disabled')
+    if (index === 0) {
+      extendArrowPrev.attr('disabled', 'disabled')
+    } else if (index + 1 === extendFlkty.slides.length) {
+      extendArrowNext.attr('disabled', 'disabled')
+    }
+  })
+  extendArrowPrev.on('click', function() {
+    extendFlkty.previous()
+  })
+  extendArrowNext.on('click', function() {
+    extendFlkty.next()
+  })
+
+  // floating chillax & relax
+  const floats = $('.index-float-chillax, .index-float-relax')
+  $(window).on('scroll', function () {
+    const deltaY = 0 - this.pageYOffset / 5
+    floats.css('transform', `translate(0, ${deltaY}px)`)
+  })
+
+  // fixed sider
+  const siders = $('.index-sider-left, .index-sider-right')
+  siders.scrollView({
+    start: '.index-classes',
+    threshold: 0,
+    end: '.footer-content-wrapper',
+    repeat: true
+  })
+
+  // scroll-view
+  const scrollViewer = $('[data-scrollview-enable]')
+  scrollViewer.scrollView({
+    threshold: 0 - window.innerHeight * 0.25
+  })
+})
